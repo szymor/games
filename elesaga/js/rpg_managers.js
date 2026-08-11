6069ba65a205eed63284b1cc3f56be92aaf438ba
vamastah <szymorawski@gmail.com>
2026-08-12 00:04:53 +0200

    fix sound for elesaga on mobiles

--- elesaga/js/rpg_managers.js
+++ elesaga/js/rpg_managers.js
@@ -1482,7 +1482,7 @@ AudioManager.updateBufferParameters = function(buffer, configVolume, audio) {
 };
 
 AudioManager.audioFileExt = function() {
-    if (WebAudio.canPlayOgg() && !Utils.isMobileDevice()) {
+    if (WebAudio.canPlayOgg()) {
         return '.ogg';
     } else {
         return '.m4a';
